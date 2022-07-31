05fd9c54f3ff7c7274f3cc87417acc61e903955c
Juan C Parra <juancparra@FACTORED-FX7HV2DYVC.local>
2022-07-31 17:19:03 -0500

    Add html templates

---
+++ website/auth.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html", text="Testing", boolean=True)
+
+
+@auth.route('/logout')
+def logout():
+    return render_template("extended_base.html",
+                           title="Log out",
+                           content="This is the log out menu")
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 characters',
+                  category='error')
+        elif password1 != password2:
+            flash('Password dont match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            flash('Account created!', category='sucess')
+
+    return render_template("sign_up.html")
